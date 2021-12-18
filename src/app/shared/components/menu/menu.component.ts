ee01f10e391cb5a6ec190ba3211d4e5fb7860ac8
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-18 16:34:32 -0600

    menú de la aplicación

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../../interfaces/menu-item';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class MenuComponent implements OnInit {
+
+  // Definición de los elementos de menú de navegación
+  menuItems: MenuItem[] = [
+    { ruta: 'mapas/fullscreen', nombre: 'FullScreen' },
+    { ruta: 'mapas/zoom-range', nombre: 'Zoom Range' },
+    { ruta: 'mapas/marcadores', nombre: 'Marcadores' },
+    { ruta: 'mapas/propiedades', nombre: 'Propiedades' }
+  ]
 
   constructor() { }
 
